5032a5d792032f6a7546343ca801e6e443946752
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2023-07-27 16:41:50 -0500

    update routing with go router

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -54,7 +55,9 @@ class _CustomListTile extends StatelessWidget {
         //   MaterialPageRoute(builder: (context) => const ButtonsScreen())
         //  );
 
-        Navigator.pushNamed(context, menuItem.url);
+        // Navigator.pushNamed(context, menuItem.url);
+
+        context.push(menuItem.url);
 
       },
     );
