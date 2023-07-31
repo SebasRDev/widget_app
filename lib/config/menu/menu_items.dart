024bba90f3fca65178331a2e22b19adac1d6af31
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2023-07-31 16:20:22 -0500

    progress indicators

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -28,4 +28,10 @@ const appMenuItems = <MenuItem>[
     url: '/cards',
     icon: Icons.credit_card
   ),
+  MenuItem(
+    title: 'Progress Indicators',
+    subtitle: 'Generales y controlados',
+    url: '/progress',
+    icon: Icons.refresh_rounded
+  ),
 ];
