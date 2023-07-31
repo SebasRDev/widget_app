c9294794d7c0e89691d25cf052fc6d40336da940
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2023-07-31 16:45:31 -0500

    Snackbars

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -34,4 +34,10 @@ const appMenuItems = <MenuItem>[
     url: '/progress',
     icon: Icons.refresh_rounded
   ),
+  MenuItem(
+    title: 'Snackbars and Dialogs',
+    subtitle: 'Screen Indicators',
+    url: '/snackbars',
+    icon: Icons.info_outline
+  ),
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -6,8 +6,31 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void showCustomSnackbar( BuildContext context ){
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hola mundo'),
+      action: SnackBarAction(label: 'Ok!', onPressed: (){}),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars adn Dialogs'),
+      ),
+      body: const Placeholder(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'), 
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
   }
 }
